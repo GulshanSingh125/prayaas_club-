2c6c4456ad749953cbde650c205fa935692f0f36
GulshanSingh125 <gulshansingh.1252005.gs@gmail.com>
2026-04-13 18:44:34 +0530

    Event and team page updated

--- src/TeamPage.jsx
+++ src/TeamPage.jsx
@@ -1,126 +1,159 @@
 import React from "react";
 import "./TeamPage.css";  // You can style this page according to your needs
-import photo1 from "./ph1.png";  // Assuming your image is named ph1.jpg (correct the file name if needed)
-import photo2 from "./myphoto.jpg"
-import ph3 from "./ph3.png"
-import ph4 from "./ph4.jpg"
-import ph5 from "./ph5.jpg"
-import ph6 from "./ph6.jpg"
-import ph7 from "./ph7.jpg"
-import ph8 from "./ph8.jpg"
-import ph9 from "./ph9.jpg"
-import ph10 from "./ph10.jpg"
-import ph11 from "./ph11.jpg"
-import ph12 from "./ph12.jpg"
-import ph14 from "./ph14.jpg"
+import photo1 from "./images/ph1.png";  // Assuming your image is named ph1.jpg (correct the file name if needed)
+import photo2 from "./images/myphoto.jpg"
+import ph3 from "./images/ph3.png"
+import ph4 from "./images/ph4.jpg"
+import ph5 from "./images/ph5.jpg"
+import ph6 from "./images/ph6.jpg"
+import ph7 from "./images/ph7.jpg"
+import ph8 from "./images/ph8.jpg"
+import ph9 from "./images/ph9.jpg"
+import ph10 from "./images/ph10.jpg"
+import ph11 from "./images/ph11.jpg"
+import ph12 from "./images/ph12.jpg"
+import ph14 from "./images/ph14.jpg"
+import ph15 from "./images/ph15.jpg"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const teamMembers = [
   {
-    name: "Gunabh Sharan",
+    name: "Harsh Rohilla",
     role: "President",
-    image: photo1,
-    linkedin: "https://www.linkedin.com/in/chirag-ramesh-chandra",
+    image: ph7,
+    linkedin: "https://www.linkedin.com/in/harshrohila78",
     twitter: "https://twitter.com/chiragramesh",
     instagram: "https://www.instagram.com/chiragramesh"
   },
   {
-    name: "Hanshika Chauhan",
+    name: "Anjali Rai",
     role: "Vice President",
-    image: ph3,
+    image: photo2,
     linkedin: "https://www.linkedin.com/in/hanshika-chauhan-1a57242a8/",
     twitter: "https://twitter.com/vaibhavkhandelwal",
     instagram: "https://www.instagram.com/vaibhavkhandelwal"
   },
   {
-    name: "Vaibhav Khandelwal",
-    role: "Technical",
-    image: photo2,
-    linkedin: "https://www.linkedin.com/in/vaibhav-khandelwal-8686a2294/",
+    name: "Hitt Jain",
+    role: "Secretary",
+    image: ph9,
+    linkedin: "https://www.linkedin.com/in/hitt-jain-34a83a27a",
     twitter: "https://twitter.com/vaibhavkhandelwal",
     instagram: "https://www.instagram.com/vaibhavkhandelwal"
   },
   {
-    name: "Ayushi Shishodia",
-    role: "Secretary",
-    image: ph4,
-    linkedin: "https://www.linkedin.com/in/chirag-ramesh-chandra",
+    name: "Divyanshi Rajput",
+    role: "Social Media",
+    image: ph12,
+    linkedin: "https://www.linkedin.com/in/divyanshi-rajput-a60791296",
     twitter: "https://twitter.com/chiragramesh",
-    instagram: "https://www.instagram.com/chiragramesh"
+    instagram: "https://www.instagram.com/_divya_rajput_2550"
   },
   {
-    name: "Kirti Sharma",
-    role: "Designer",
+    name: "Palak Mishra",
+    role: "Social Media",
     image: ph5,
     linkedin: "https://www.linkedin.com/in/hanshika-chauhan-1a57242a8/",
     twitter: "https://twitter.com/vaibhavkhandelwal",
     instagram: "https://www.instagram.com/vaibhavkhandelwal"
   },
   {
-    name: "Aditi Sinha",
-    role: "Designer",
+    name: "Abhinav",
+    role: "Social Media",
     image: ph6,
     linkedin: "https://www.linkedin.com/in/vaibhav-khandelwal-8686a2294/",
     twitter: "https://twitter.com/vaibhavkhandelwal",
     instagram: "https://www.instagram.com/vaibhavkhandelwal"
   },
   {
-    name: "Harsh Rohilla",
-    role: "Technical",
+    name: "Aryan Yadav",
+    role: "Document",
     image: ph7,
     linkedin: "https://www.linkedin.com/in/vaibhav-khandelwal-8686a2294/",
     twitter: "https://twitter.com/vaibhavkhandelwal",
     instagram: "https://www.instagram.com/vaibhavkhandelwal"
   },
   {
-    name: "Tannu Choudhary",
-    role: "Treasurer",
+    name: "Ujjwal",
+    role: "Document",
     image: ph8,
     linkedin: "https://www.linkedin.com/in/vaibhav-khandelwal-8686a2294/",
     twitter: "https://twitter.com/vaibhavkhandelwal",
     instagram: "https://www.instagram.com/vaibhavkhandelwal"
   },
   {
-    name: "Hitt Jain",
-    role: "Outreach & PR",
+    name: "Priyanjan Kumar Singh",
+    role: "Event Management",
     image: ph9,
     linkedin: "https://www.linkedin.com/in/vaibhav-khandelwal-8686a2294/",
     twitter: "https://twitter.com/vaibhavkhandelwal",
     instagram: "https://www.instagram.com/vaibhavkhandelwal"
   },
   {
-    name: "Ujjaval Mishra",
-    role: "Outreach & PR",
+    name: "Rahul Gupta",
+    role: "Event Management",
     image: ph11,
     linkedin: "https://www.linkedin.com/in/ujjaval-mishra-6974072a2?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
     twitter: "https://x.com/ArmyUjjaval?t=j4Dt4X3CuAr9LXkYwuTYbg&s=09",
     instagram: "https://www.instagram.com/ujjavalmishra9918/profilecard/?igsh=MXNjZ3p6aGd4ZGJ5Mg=="
   },
   {
-    name: "Divyanshi Rajput",
-    role: "Social Media",
+    name: "Vyom Varshney",
+    role: "Event Management",
     image: ph12,
     linkedin: "https://www.linkedin.com/in/divyanshi-rajput-a60791296?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
     twitter: "https://twitter.com/vaibhavkhandelwal",
     instagram: "https://www.instagram.com/_divya_rajput_2550?igsh=ZjQya2Jnd3BkcTZv"
   },
   {
-    name: "Nancy Bansal",
-    role: "Social Media",
+    name: "Akshita",
+    role: "Event Management",
     image: ph14,
     linkedin: "https://www.linkedin.com/in/nancy-bansal-267710279?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
     twitter: "https://twitter.com/vaibhavkhandelwal",
     instagram: "https://www.instagram.com/nanc_yy7/"
   },
   {
-    name: "Palak Chauhan",
-    role: "Documentation",
+    name: "Ayush Trivedi",
+    role: "PR & Outreach",
     image: ph10,
     linkedin: "https://www.linkedin.com/in/palak-chauhan-ab5b20325?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app ",
     twitter: "https://twitter.com/vaibhavkhandelwal",
     instagram: "https://www.instagram.com/_palak_1223/profilecard/?igsh=MWVrZXc4ZWJubWdmdg=="
-  }
+  },
+  {
+    name: "Raghav",
+    role: "Design",
+    image: ph10,
+    linkedin: "https://www.linkedin.com/in/raghava-m-vasudevan",
+    twitter: "https://twitter.com/vaibhavkhandelwal",
+    instagram: "https://www.instagram.com/_palak_1223/profilecard/?igsh=MWVrZXc4ZWJubWdmdg=="
+  },
+  {
+    name: "Pavni Goyal",
+    role: "Design",
+    image: ph10,
+    linkedin: "https://www.linkedin.com/in/palak-chauhan-ab5b20325?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app ",
+    twitter: "https://twitter.com/vaibhavkhandelwal",
+    instagram: "https://www.instagram.com/_palak_1223/profilecard/?igsh=MWVrZXc4ZWJubWdmdg=="
+  },
+  {
+    name: "Abhishek Rai",
+    role: "Technical",
+    image: ph10,
+    linkedin: "https://www.linkedin.com/in/palak-chauhan-ab5b20325?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app ",
+    twitter: "https://twitter.com/vaibhavkhandelwal",
+    instagram: "https://www.instagram.com/_palak_1223/profilecard/?igsh=MWVrZXc4ZWJubWdmdg=="
+  },
+  {
+    name: "Gulshan Singh",
+    role: "Technical",
+    image: ph15,
+    linkedin: "https://www.linkedin.com/in/palak-chauhan-ab5b20325?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app ",
+    twitter: "https://twitter.com/vaibhavkhandelwal",
+    instagram: "https://www.instagram.com/_palak_1223/profilecard/?igsh=MWVrZXc4ZWJubWdmdg=="
+  },
 ];
 
 const TeamPage = () => {
--- src/event.jsx
+++ src/event.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import "./event.css";
 
 // Importing images directly from the src folder
-import elc1 from "./elc1.jpg";
-import elc7 from "./ELC7.jpg";
-import elc2 from "./elc2.jpg";
-import elc4 from "./elc4.jpg";
-import elc3 from "./elc3.jpg";
-import elc5 from "./elc5.jpg";
+import elc1 from "./images/elc1.jpg";
+import elc7 from "./images/elc7.jpg";
+import elc2 from "./images/elc2.jpg";
+import elc4 from "./images/elc4.jpg";
+import elc3 from "./images/elc3.jpg";
+import elc5 from "./images/elc5.jpg";
+import elc9 from "./images/elc9.jpg";
+import elc10 from "./images/elc10.jpg";
+import elc11 from "./images/elc11.jpg";
+import elc12 from "./images/elc12.jpg";
+import elc13 from "./images/elc13.jpg";
+import elc14 from "./images/elc14.jpg";
+
+
 
 const PrayaasELC = () => {
   return (
@@ -53,7 +61,34 @@ const PrayaasELC = () => {
         <h2>Recent Club Events</h2>
 
         <div className="event">
-          <h3>Event 1: VIKSIT BHARAT</h3>
+          <h3>Event 1: ELECTIONS FOR CR</h3>
+          <p>
+            Promotes leadership and responsibility among students by giving them a real experience of the electoral process.
+            <br />
+            "Encourages active participation in decision-making and strengthens democratic values within the student community."
+          </p>
+          <div className="event-images">
+            <img src={elc10} alt="Electoral Awareness Workshop" />
+            <img src={elc9} alt="Workshop Session" />
+          </div>
+        </div>
+
+        <div className="event">
+        <h3>Event 2: Empowering youth through Awareness</h3>
+        <p>
+          To educate students about the Special Intensive Revision (SIR) process and its role in maintaining accurate electoral records.
+          <br />
+          "To promote democratic awareness, responsible citizenship, and active participation in the electoral system."
+        </p>
+        <div className="event-images">
+          <img src={elc11} alt="Electoral Awareness Workshop" />
+          <img src={elc12} alt="Workshop Session" />
+        </div>
+
+
+        </div>
+    {/* <div className="event">  
+          <h3>Event 3: VIKSIT BHARAT</h3>
           <p>
             "Vikshit Bharat" translates to "Developed India" in English. Here's a two-line description:
             <br />
@@ -66,8 +101,10 @@ const PrayaasELC = () => {
           </div>
         </div>
 
+
+
         <div className="event">
-          <h3>Event 2: PEHLA VOTE DESH KE LIYE</h3>
+          <h3>Event 4: PEHLA VOTE DESH KE LIYE</h3>
           <p>
             "Pehla Vote Desh Ke Liye" symbolizes the power of youth in shaping the future of our country through active
             participation in elections.
@@ -81,7 +118,7 @@ const PrayaasELC = () => {
         </div>
 
         <div className="event">
-          <h3>Event 3: SAY OUT LOUD</h3>
+          <h3>Event 5: SAY OUT LOUD</h3>
           <p>Got something to say? Come, say it here! Presenting: SAY OUT LOUD!</p>
           <div className="event-images">
             <img src={elc3} alt="Voter Registration Drive" />
@@ -89,7 +126,7 @@ const PrayaasELC = () => {
         </div>
 
         <div className="event">
-          <h3>Event 4: YUVA MANTHAN</h3>
+          <h3>Event 6: YUVA MANTHAN</h3>
           <p>
             Championing intellectual prowess and diplomatic finesse, the triumphant culmination of YUVA MANTHAN - MODEL
             UNITED NATION, marks a monumental stride towards nurturing visionary leaders of tomorrow.
@@ -97,7 +134,7 @@ const PrayaasELC = () => {
           <div className="event-images">
             <img src={elc5} alt="Debate on Electoral Reforms" />
           </div>
-        </div>
+        </div>   */} 
       </section>
     </div>
   );
